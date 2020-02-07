87a1d26fbb07c98d08fe9ab57073e4faa500d86a
Doug Kintop <dkintop@hotmail.com>
2020-02-07 12:39:18 -0500

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,33 @@
 // TODO: Create action creators as defined in tests
+export const addQuote = quote => {
+  return {
+    type: "ADD_QUOTE",
+    quote: {
+      content: quote.content,
+      author: quote.author,
+      votes: quote.votes,
+      id: uuid()
+    }
+  };
+};
+
+export const removeQuote = quoteId => {
+  return {
+    type: "REMOVE_QUOTE",
+    quoteId
+  };
+};
+
+export const upvoteQuote = quoteId => {
+  return {
+    type: "UPVOTE_QUOTE",
+    quoteId
+  };
+};
+
+export const downvoteQuote = quoteId => {
+  return {
+    type: "DOWNVOTE_QUOTE",
+    quoteId
+  };
+};
