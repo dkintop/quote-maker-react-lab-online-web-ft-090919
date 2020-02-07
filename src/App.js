20125437fcaf8f1f1900d1ea6e9cc9a37e161af1
Doug Kintop <dkintop@hotmail.com>
2020-02-07 12:44:07 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ class App extends Component {
         </div>
         <hr />
 
-        {/* <QuoteForm />
-        <Quotes /> */}
+        <QuoteForm />
+        {/* <Quotes /> */}
       </div>
     );
   }
